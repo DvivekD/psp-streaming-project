d3ccb73691fe8fe834b88c82be072dc5d5a802cc
DvivekD <DvivekD@users.noreply.github.com>
2026-05-27 23:34:50 +0530

    fix: rewrite CloudMedia.js to use GoTube SiteList.push format

--- psp_plugins/CloudMedia.js
+++ psp_plugins/CloudMedia.js
@@ -1,73 +1,85 @@
-name = "Cloud Media (Anime & Movies)";
-serverUrl = "http://20.24.21.215:8082";
+var CloudMedia = new Object();
+CloudMedia.rev = 1;
+CloudMedia.SearchDesc = "Anime and Movies";
+CloudMedia.Name = "CloudMedia";
 
-function search(query, isMovie) {
-    var type = isMovie ? "movie" : "anime";
-    var searchUrl = serverUrl + "/search_media?type=" + type + "&query=" + escape(query);
+CloudMedia.Search = function (keyword, page) {
+    var result = new Object();
+    result.bypage = 20;
+    result.start = (page - 1) * result.bypage + 1;
     
-    // Fetch JSON from Azure
-    var jsonString = util.getURL(searchUrl); 
-    if (!jsonString || jsonString.indexOf("success") === -1) {
-        gotube.addList("Search Failed or Timed Out", "", "", "");
-        return;
-    }
-    
-    var response = eval("(" + jsonString + ")"); 
-    
-    if (response.success && response.data && response.data.length > 0) {
-        for (var i = 0; i < response.data.length; i++) {
-            var item = response.data[i];
-            var img = item.image || item.img || "";
-            // title, function, argument, thumbnail
-            gotube.addList(item.title, "playMedia", type + "||" + item.id, img);
-        }
-    } else {
-        gotube.addList("No results found.", "", "", "");
-    }
-}
-
-// In GoTube menus, we need a way to distinct a search between Anime and Movies.
-// We will just expose Anime for now as default, or we can use the plugin's built-in hooks if available.
-// Actually, GoTube passes query to `search` natively. 
-// We will just assume anime for default search unless they type "movie:" prefix.
-function search_override(query) {
     var isMovie = false;
-    if (query.toLowerCase().indexOf("movie:") === 0) {
+    if (keyword.charAt(0) == '$') {
         isMovie = true;
-        query = query.substring(6).replace(/^\s+/, "");
-    } else if (query.toLowerCase().indexOf("anime:") === 0) {
-        query = query.substring(6).replace(/^\s+/, "");
+        keyword = keyword.substring(1).replace(/^\s+/, "");
     }
-    search(query, isMovie);
+    
+    var type = isMovie ? "movie" : "anime";
+    var serverUrl = "http://20.24.21.215:8082";
+    var searchUrl = serverUrl + "/search_media?type=" + type + "&query=" + escape(keyword);
+    
+    PSPTube.log("CloudMedia Search: " + searchUrl + "\n");
+    
+    var jsonString = GetContents(searchUrl);
+    
+    result.VideoInfo = new Array();
+    result.total = 0;
+    
+    // Fallback error item
+    var err = {attr:2};
+    err.id = "0";
+    err.Title = "CloudMedia Search Info";
+    err.Description = "Prefix with $ to search Movies instead of Anime. (e.g., $oppenheimer)";
+    err.ThumbnailURL = "";
+    err.SaveFilename = "info.flv";
+    err.URL = "";
+    result.VideoInfo.push(err);
+
+    if (jsonString && jsonString.indexOf("success") !== -1) {
+        var response = eval("(" + jsonString + ")");
+        if (response.success && response.data) {
+            result.total = response.data.length;
+            for (var i = 0; i < response.data.length; i++) {
+                var item = response.data[i];
+                var v = {attr:2};
+                v.id = item.id;
+                v.Title = item.title || "Unknown Title";
+                v.Description = type.toUpperCase() + "\n" + (item.releaseDate || item.type || "");
+                v.ThumbnailURL = item.image || item.img || "";
+                v.SaveFilename = v.id + ".flv";
+                // Pass type alongside id
+                v.URL = 'CloudMedia.play("' + type + '||' + v.id + '")';
+                result.VideoInfo.push(v);
+            }
+        }
+    }
+    
+    result.end = result.start - 1 + result.VideoInfo.length;
+    return result;
 }
 
-// Standard GoTube function hook
-search = search_override;
-
-function playMedia(args) {
+CloudMedia.play = function (args) {
     var splitArgs = args.split("||");
     var type = splitArgs[0];
     var id = splitArgs[1];
-    
-    // For Movies, id and episodeId are usually the same or just passing id works.
-    // For Anime, we need to fetch episodes. Since GoTube JS is simple, we will automatically play Episode 1
-    // or fetch the episode list. To keep it simple, we just play the first episode/media directly.
     var episodeId = id; 
     
     if (type === "anime") {
-        // Gogoanime episode IDs are usually id + "-episode-1"
         episodeId = id + "-episode-1";
     }
 
+    var serverUrl = "http://20.24.21.215:8082";
     var linkUrl = serverUrl + "/get_stream_link?type=" + type + "&id=" + escape(id) + "&episodeId=" + escape(episodeId);
-    var rawM3u8 = util.getURL(linkUrl);
+    
+    PSPTube.log("Fetching stream: " + linkUrl + "\n");
+    var rawM3u8 = GetContents(linkUrl);
     
     if (!rawM3u8 || rawM3u8 === "ERROR") {
-        gotube.addList("Failed to extract stream.", "", "", "");
-        return;
+        return ""; // Fails silently on PSP
     }
     
-    // Pass the raw link to the ffmpeg pipe on Azure
     var playUrl = serverUrl + "/play?url=" + escape(rawM3u8) + "&id=" + escape(id);
-    gotube.play(playUrl);
+    return playUrl;
 }
+
+SiteList.push(CloudMedia);
